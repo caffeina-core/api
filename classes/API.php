84c64e39d96889e6c75c4df5f3e98af68ec687ea
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2014-11-17 18:27:02 +0100

    Multiple version support
    Signed-off-by: Stefano Azzolini <stefano.azzolini@caffeinalab.com>

--- classes/API.php
+++ classes/API.php
@@ -19,22 +19,21 @@ class API {
   public static function run(callable $main = null){
     $API_VERS = Options::get('base.api_version','');
     // Load Routes
-    $route_file = rtrim(Options::get('base.endpoints',APP_DIR.'/routes.php'),'/');
+    $route = rtrim(Options::get('base.endpoints',APP_DIR.'/routes.php'),'/');
     // Single file
-    if (is_file($route_file)){
-      include $route_file;
+    if (is_file($route)){
+      include $route;
     } else {
       // Load directory
-      $route_file .= rtrim('/'.$API_VERS,'/');
-      if (is_dir($route_file)){
-          Route::group("/$API_VERS",function() use ($route_file,$API_VERS){
-            Route::any('/',function() use ($API_VERS){
-              Event::trigger('api.index',[$API_VERS]);              
+      foreach((array)$API_VERS as $API_NAMESPACE){
+        $routes = $route . rtrim('/'.$API_NAMESPACE,'/');
+        if (is_dir($routes)){
+            Route::group("/$API_NAMESPACE",function() use ($routes,$API_NAMESPACE){
+              Event::trigger('api.before');
+              array_map(function($f){include $f;},glob($routes.'/*.php'));
+              Event::trigger('api.after');
             });
-            Event::trigger('api.before');
-            array_map(function($f){include $f;},glob($route_file.'/*.php'));
-            Event::trigger('api.after');
-          });
+        }        
       }
     }
     Event::trigger('api.run',[Options::get('base.api_version','')]);
